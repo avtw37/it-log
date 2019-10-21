bfa57b2792985ae4ee05a43e0cb8f4d65432e815
Abel Dimas <avtw37@gmail.com>
2019-10-21 11:39:14 -0700

    Updates

---
+++ precache-manifest.0c48686827e68438e99c86eb41ccb75e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76e6a8b07f2ea2cc699efecd43298238",
+    "url": "/it-log/index.html"
+  },
+  {
+    "revision": "e8d60fff58f02dd08b99",
+    "url": "/it-log/static/css/2.8bed955a.chunk.css"
+  },
+  {
+    "revision": "3bc5acadf1c6caabe0d6",
+    "url": "/it-log/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "e8d60fff58f02dd08b99",
+    "url": "/it-log/static/js/2.b0e8f0d4.chunk.js"
+  },
+  {
+    "revision": "3bc5acadf1c6caabe0d6",
+    "url": "/it-log/static/js/main.87fee3aa.chunk.js"
+  },
+  {
+    "revision": "6c45740ea7d7673daa80",
+    "url": "/it-log/static/js/runtime-main.4a2f6ab5.js"
+  }
+]);
