d000ddd496895ac1aca780165f2fc041d5f6c15f
Abel Dimas <avtw37@gmail.com>
2019-10-21 11:55:34 -0700

    added status to actions

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -25,7 +25,7 @@ export const getLogs = () => async dispatch => {
   } catch (err) {
     dispatch({
       type: LOGS_ERROR,
-      payload: err.response
+      payload: err.response.status
     });
   }
 };
@@ -51,7 +51,7 @@ export const addLog = log => async dispatch => {
   } catch (err) {
     dispatch({
       type: LOGS_ERROR,
-      payload: err.response
+      payload: err.response.status
     });
   }
 };
@@ -72,7 +72,7 @@ export const deleteLog = id => async dispatch => {
   } catch (err) {
     dispatch({
       type: LOGS_ERROR,
-      payload: err.response
+      payload: err.response.status
     });
   }
 };
@@ -99,7 +99,7 @@ export const updateLog = log => async dispatch => {
   } catch (err) {
     dispatch({
       type: LOGS_ERROR,
-      payload: err.response
+      payload: err.response.status
     });
   }
 };
@@ -127,7 +127,7 @@ export const searchLogs = (text) => async dispatch => {
   } catch (err) {
     dispatch({
       type: LOGS_ERROR,
-      payload: err.response
+      payload: err.response.status
     });
   }
 };
